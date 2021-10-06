e90637cfa24516948b76a02ef25921a9aac6a23d
Salman Nazeer <83122098+salmanselu@users.noreply.github.com>
2021-10-06 15:28:54 +0530

    Update factorial.c
    Refactored the factorial function to be simpler

--- factorial.c
+++ factorial.c
@@ -3,19 +3,14 @@
 long int Factorial( int ) ;
 
 void main(void)
-  {
+ {
     int n ;
     printf("Input a number for a factorial value \n");
     scanf("%d", &n);
     printf("%ld \n", Factorial(n) ) ;
 }
 
-long int Factorial( int n )
-{
-  long int Result ;
-  if ( n == 0 )
-    Result = 1 ;
-  else
-    Result = n * Factorial( n-1 ) ;
-  return( Result ) ;
+long int Factorial(int n){
+    if(n == 0) return 1;
+    return n * Factorial(n - 1);
 }
